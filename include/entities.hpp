ca23d55222f5d46fec48bcd729aecd277edaa2d0
Willem Melis <willem.melis@outlook.com>
2021-06-05 16:53:06 +0200

    Add draft panoc
    * Add draft implementation of panoc,
    it appears some changes in proximal gradient algo are required.

--- include/entities.hpp
+++ include/entities.hpp
@@ -1,30 +1,48 @@
-#pragma once
+﻿#pragma once
 
 #include<utility>
 
 namespace pnc {
 
-template <
-    typename TVectorRef,
-    typename TVector = std::remove_reference_t<TVectorRef>,
-    typename TConstant = typename TVector::data_type
-    >
-struct Location {
-    Location (
-            TVectorRef&& location,
-            TVectorRef&& gradient,
-            TConstant cost,
-            TConstant gamma) :
-        location(std::forward<TVectorRef>(location)),
-        gradient(std::forward<TVectorRef>(gradient)),
-        gamma(gamma),
-        cost(cost)
-    { } 
+	template <
+		typename TVectorRef,
+		typename TVector = std::remove_reference_t<TVectorRef>,
+		typename TConstant = typename TVector::data_type
+	>
+		struct Location {
+		Location(
+			TVectorRef&& location,
+			TVectorRef&& gradient,
+			TConstant cost,
+			TConstant gamma) :
+			location(std::forward<TVectorRef>(location)),
+			gradient(std::forward<TVectorRef>(gradient)),
+			gamma(gamma),
+			cost(cost)
+		{ }
 
-    TVectorRef location;
-    TVectorRef gradient;
-    TConstant gamma;
-    TConstant cost;
-};
+		Location() :
+			location(),
+			gradient(),
+			gamma(0),
+			cost(0)
+		{
+		}
+
+		template<typename TVec>
+		Location(const Location<TVec>& other)
+			:
+			location(other.location),
+			gradient(other.gradient),
+			gamma(other.gamma),
+			cost(other.cost)
+		{
+		}
+
+		TVectorRef location;
+		TVectorRef gradient;
+		TConstant gamma;
+		TConstant cost;
+	};
 
 }
