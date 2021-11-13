4f077f53de18602a99df3bdea942e1c380f411ba
Willem Melis <willem.melis@outlook.com>
2021-11-13 23:30:06 +0100

    Update readme and examples.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
  # PANOCPP
-Panoc algorithm in c++, the goal is to provide a single header file that contains a working high performant panoc ssolver.
+Panoc algorithm in c++, the goal is to provide a single header file that contains a working high performant panoc solver.
 
-You can find the prototype for this solver [here at zilleplus/panocsharp](http://github.com/Zilleplus/PANOCSharp).
+You can find a first version of the solver at build-header/panocpplib.hpp. And an example at build-header/exampleBox.cpp
+
+compile the examples with the panocpplib in the same directory and C++17 enabled:
+msvc: cl /std:17 exampleBox.cpp 
+clang++: clang++ -std=c++17 exampleBox.cpp
+g++: g++ -std=c++17 exampleBox.cpp
+
+At this stage the solver works, but does not have the optimal performance yet. The current goal is to have a production grade version by the end of 2021.
---
+++ build-header/exampleBox.cpp
@@ -0,0 +1,34 @@
+#include "panocpplib.hpp"
+
+int main(){
+	constexpr int number_of_iterations = 5;
+	constexpr int degree = 6;
+	constexpr int dimension = 2;
+
+	auto cost_function = [degree](const auto& input, auto& output)
+	{
+		const auto cost = pow(input[0], degree) + pow(input[1], degree);
+		output[0] = degree*pow(input[0], degree-1);
+		output[1] = degree*pow(input[1], degree-1);
+		
+		return cost;
+	};
+	double offset = 2;
+	pnc::BoxOp<double> box{ -4.0,4.0 };
+
+	pnc::PanocConfig config = {
+		10, // max_iterations
+		10, // max_fbe_iterations
+		1e-15 // min_residual
+	};
+	pnc::Panoc solver = { cost_function, box, config, dimension};
+
+	pnc::Vector<double> solution = { 1,1 }; 
+	solver.Solve(solution);
+
+	std::cout << "x[0]=" << solution[0]; // 0.204269
+    std::cout << " ";
+	std::cout << "x[1]=" << solution[1]; // 0.204269
+
+	return EXIT_SUCCESS;
+}
